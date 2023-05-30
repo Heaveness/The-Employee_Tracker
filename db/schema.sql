d25167c03cd2eef6101a1b9194a2364e274f9170
Heaveness <vindictuslbp@gmail.com>
2023-05-30 04:28:04 -0400

    Added code into the schema.sql for users to use to create their own local database for the app to work on local machines.

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Create a department table.
+CREATE TABLE department (
+  	id INT AUTO_INCREMENT PRIMARY KEY,
+  	name VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE department MODIFY COLUMN name VARCHAR(255);
+
+-- Create a role table.
+CREATE TABLE role (
+  	id INT AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(255) NOT NULL,
+	salary DECIMAL(10, 2) NOT NULL,
+	department_id INT,
+	FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Create a employee table.
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT,
+	manager_id INT,
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
